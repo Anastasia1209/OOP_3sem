7e3622e0d33d86a3d1d045d5b610886a2b656039
Anastasiya Golodok <nastya.golodok@mail.ru>
2022-11-02 01:14:55 +0300

    try-catch-finally, universal catch

--- Lab06/Lab06/Exception.cs
+++ Lab06/Lab06/Exception.cs
@@ -7,60 +7,60 @@ using System.Threading.Tasks;
 
 namespace Lab06
 {
-        public class MyException : System.Exception
+    public class MyException : System.Exception
+    {
+        public string ErrorClass { get; set; }
+        public MyException(string message, string errorClass)
+            : base(message)  // наследуем message от System.Exception
         {
-            public string ErrorClass { get; set; }
-            public MyException(string message, string errorClass)
-                : base(message)  // наследуем message от System.Exception
-            {
-                this.ErrorClass = errorClass;
-            }
+            this.ErrorClass = errorClass;
         }
+    }
 
-        public class CostException : MyException
-        {
+    public class CostException : MyException
+    {
 
-            public int Cost { get; set; }
-            public CostException(string message, int errorCost)
-                : base(message, "Error code 1: Uncorrect cost.\n")  // наследуем message и errorClass от MyException
-            {
-
-                this.Cost = errorCost;
-            }
-        }
-        public class ColorException : MyException
+        public int Cost { get; set; }
+        public CostException(string message, int errorCost)
+            : base(message, "Error code 1: Uncorrect cost.\n")  // наследуем message и errorClass от MyException
         {
 
-            public string Color { get; set; }
-            public ColorException(string message, string errorColor)
-                : base(message, "Error code 1: Uncorrect color.\n")  // наследуем message и errorClass от MyException
-            {
-
-                this.Color = errorColor;
-            }
+            this.Cost = errorCost;
         }
-        public class NumException : MyException
+    }
+    public class ColorException : MyException
+    {
+
+        public string Color { get; set; }
+        public ColorException(string message, string errorColor)
+            : base(message, "Error code 1: Uncorrect color.\n")  // наследуем message и errorClass от MyException
         {
 
-            public int Num { get; set; }
-            public NumException(string message, int errorNum)
-                : base(message, "Error code 1: Uncorrect number.\n")  // наследуем message и errorClass от MyException
-            {
-
-                this.Num = errorNum;
-            }
+            this.Color = errorColor;
         }
-        public class TypeException : MyException
+    }
+    public class NumException : MyException
+    {
+
+        public int Num { get; set; }
+        public NumException(string message, int errorNum)
+            : base(message, "Error code 1: Uncorrect number.\n")  // наследуем message и errorClass от MyException
         {
 
-            public int Type { get; set; }
-            public TypeException(string message, int errorType)
-                : base(message, "Error code 1: Uncorrect type.\n")  // наследуем message и errorClass от MyException
-            {
-
-                this.Type = errorType;
-            }
+            this.Num = errorNum;
         }
+    }
+    public class TypeException : MyException
+    {
+
+        public int Type { get; set; }
+        public TypeException(string message, int errorType)
+            : base(message, "Error code 1: Uncorrect type.\n")  // наследуем message и errorClass от MyException
+        {
+
+            this.Type = errorType;
+        }
+    }
     public class ConsoleLogger
     {
         public ConsoleLogger() { }
@@ -96,7 +96,7 @@ namespace Lab06
         public FileLogger() { }
         public void WriteLog(MyException exception)
         {
-            CostException CostEx = exception as  CostException;
+            CostException CostEx = exception as CostException;
             ColorException ColorEx = exception as ColorException;
             NumException NumEx = exception as NumException;
             TypeException TypeEx = exception as TypeException;
@@ -122,4 +122,48 @@ namespace Lab06
             }
         }
     }
-} 
+    public class Num
+    {
+        public int num { get; set; }
+        public Num(int num)
+        {
+            this.num = num;
+        }
+        public Num Namnamnam()
+        {
+            try
+            {
+                if (!(this.num is int))
+                {
+                    throw new MyException("Incorrect type", "Error code 5: Uncorrect type.\n");
+                }
+
+                return this;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public int Omnomnom()
+        {
+            try
+            {
+                if (Convert.ToInt32(this.Namnamnam()) is int)
+                {
+                    return Convert.ToInt32(this.Namnamnam());
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message + "impossible convert to int\n");
+                return 0;
+            }
+        }
+    }
+}
--- Lab06/Lab06/Program.cs
+++ Lab06/Lab06/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace Lab06
 {
@@ -7,38 +8,6 @@ namespace Lab06
     {
         static void Main(string[] args)
         {
-          /*  Bush bush = new Bush("1,5m", "Raspberry", "July-August");
-            Flower flow = new Flower("yellow", "June");
-            Cactus cactus = new Cactus(1500, "September");
-
-            Plant pl = new Bush("1", "Currant", "June");
-            Console.WriteLine(pl.ToString());
-
-            // Bouquet bouquet = new Bouquet(20);
-            Rose rose = new Rose("red", 10);
-            Rose rose1 = new Rose("white", 15);
-            Rose rose2 = new Rose("pink", 9);
-            //  bouquet = rose as Bouquet;
-            if (rose is Bouquet)
-            {
-                Console.WriteLine("YES!");
-            }
-
-            Gladiolus gladiolus1 = new Gladiolus(5, 6);
-            Gladiolus gladiolus2 = new Gladiolus(6, 9);
-            Gladiolus gladiolus3 = new Gladiolus(3, 5);
-
-            Paper paper1 = new Paper("Organza", 3);
-            Paper paper2 = new Paper("Netting", 2);
-            Paper paper3 = new Paper("Felt", 4);
-            Paper paper4 = new Paper("Paper", 4);
-
-            Container bouquet1 = new Container();
-            bouquet1.Add(paper1);
-            bouquet1.Add(rose1);
-            bouquet1.Add(rose2);
-            bouquet1.Add(gladiolus1);*/
-
             FileLogger fileLogger = new FileLogger();
             try
             {
@@ -73,6 +42,31 @@ namespace Lab06
             {
                 Console.WriteLine("Программа завершена успешно!!");
             }
+
+            try
+            {
+                int x = 5, y = 0;
+                int c = x / y;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message + "\n");
+            }
+
+            try
+            {
+                int[] arr = new int[8];
+                arr[10] = 10;
+            }
+            catch (Exception e) when (e.Source != "Lab06")
+            {
+                Console.WriteLine("ок\n");
+            }
+            catch
+            {
+                Console.WriteLine("Ошиииибка:(" + "\n");
+            }
+
         }
     }
 }
