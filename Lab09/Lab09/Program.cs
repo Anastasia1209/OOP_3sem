f14a3b6fa76c74431e3d01d6ec01c9df0f181678
Anastasiya Golodok <nastya.golodok@mail.ru>
2022-11-15 02:20:12 +0300

    MyHashtable, method Add

---
+++ Lab09/Lab09/Program.cs
@@ -0,0 +1,66 @@
+﻿using Lab09;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+namespace lab09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           
+
+            Hashtable hashtable = new Hashtable();
+            hashtable.Add(12, "aaa");
+            Console.WriteLine(hashtable[12]);
+            hashtable.Add(6, 'k');
+            Console.WriteLine(hashtable[6]);
+            hashtable.Add(12.6, true);
+            Console.WriteLine(hashtable[12.6]);
+            hashtable.Add("rr", false);
+            Console.WriteLine(hashtable["rr"]);
+
+            Console.WriteLine("-------------------");
+
+            ArrayList arr = new ArrayList();
+            arr.Add(hashtable[12]);
+            arr.Add(hashtable[6]);
+            arr.Add(hashtable[12.6]);
+            arr.Add(hashtable["rr"]);
+            foreach(var temp in arr)
+            {
+                Console.WriteLine(temp);
+            }
+
+            string str = "aaa";
+            Console.WriteLine("Содержит ли ArrayList заданное значение " + arr.Contains(str));
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            var myCollection = new ObservableCollection<Worker>();
+            myCollection.CollectionChanged += SayChange;
+
+
+            myCollection.Add(new Worker("600$", "1 year"));
+            myCollection.Add(new Worker("1000$", "2 year"));
+            myCollection.Add(new Worker("100000000$", "15 year"));
+
+            myCollection.RemoveAt(2);
+
+        }
+        private static void SayChange(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (e.Action == NotifyCollectionChangedAction.Add)
+                Console.WriteLine("|Add complete|");
+            else if (e.Action == NotifyCollectionChangedAction.Remove) Console.WriteLine("|Remove complete|");
+        }
+    }
+}
---
+++ Lab09/Lab09/Worker.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab09
+{
+    public class Worker
+    {
+        public string salary;
+        public string experience;
+
+        public Worker(string sal, string exp)
+        {
+            salary = sal;
+            experience = exp;
+        }
+    }
+    public class MyHashtable : IEnumerable<Worker>
+    {
+        public class Item
+        {  
+            public string Key { get; private set; }
+
+            public string Value { get; private set; }
+
+            public Item(Worker itemWorker)
+            {
+                if (string.IsNullOrEmpty(itemWorker.salary))
+                {
+                    throw new ArgumentNullException(nameof(itemWorker.salary));
+                }
+
+                if (string.IsNullOrEmpty(itemWorker.experience))
+                {
+                    throw new ArgumentNullException(nameof(itemWorker.experience));
+                }
+
+                Key = itemWorker.salary;
+                Value = itemWorker.experience;
+            }
+
+            public Item(string key, string value)
+            {
+                if (string.IsNullOrEmpty(key))
+                {
+                    throw new ArgumentNullException(nameof(key));
+                }
+
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException(nameof(value));
+                }
+
+                Key = key;
+                Value = value;
+            }
+
+            public override string ToString() => Key;
+        }
+   
+        private readonly byte _maxSize = 255;
+        
+        private Dictionary<int, List<Item>> _items = null;
+    
+        public IReadOnlyCollection<KeyValuePair<int, List<Item>>> Items => _items.ToList().AsReadOnly();
+
+
+        public MyHashtable()
+        {
+           
+            _items = new Dictionary<int, List<Item>>(_maxSize);
+        }
+
+        public void Add(string key, string value)
+        {
+         
+            IsNullOrEmpty(key);
+
+            if (key.Length > _maxSize)
+            {
+                throw new ArgumentException($"The maximum key length is {_maxSize} characters.", nameof(key));
+            }
+
+            IsNullOrEmpty(value);
+
+            var item = new Item(key, value);
+
+            var hash = GetHash(item.Key);
+
+            List<Item> hashTableItem = null;
+            if (_items.ContainsKey(hash))
+            {
+                hashTableItem = _items[hash];
+
+                var oldElementWithKey = hashTableItem.SingleOrDefault(x => x.Key == item.Key);
+                if (oldElementWithKey != null)
+                {
+                    throw new ArgumentException($"The hash table already contains an item with a key {key}. The key must be unique.", nameof(key));
+                }
+
+                _items[hash].Add(item);
+            }
+            else
+            {
+                hashTableItem = new List<Item> { item };
+                _items.Add(hash, hashTableItem);
+            }
+        }
+
+        public void Add(Worker itemWorker)
+        {
+            var item = new Item(itemWorker);
+
+            var hash = GetHash(item.Key);
+
+            List<Item> hashTableItem = null;
+            if (_items.ContainsKey(hash))
+            {
+                hashTableItem = _items[hash];
+
+                var oldElementWithKey = hashTableItem.SingleOrDefault(x => x.Key == item.Key);
+                if (oldElementWithKey != null)
+                {
+                    throw new ArgumentException($"The hash table already contains an item with a key {itemWorker.salary}. The key must be unique.", nameof(itemWorker.salary));
+                }
+
+                _items[hash].Add(item);
+            }
+            else
+            {
+                hashTableItem = new List<Item> { item };
+                _items.Add(hash, hashTableItem);
+            }
+        }
+        //---------------------------
+        
+    }
+}
