bb82847f2088038e8896055b8e0248805358428c
Anastasiya Golodok <nastya.golodok@mail.ru>
2022-12-12 23:28:46 +0300

    (de)serialization soap, xml, json, binary, XPath

---
+++ Lab13/Lab13/Bouquet.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab13
+{
+    [Serializable]
+    public class Bouquet
+    {
+        private int costOfBouquet;
+        public int CostOfBouquet
+        {
+            get { return costOfBouquet; }
+            set { costOfBouquet = value; }
+        }
+
+        public Bouquet()
+        {
+            costOfBouquet = 100;
+
+        }
+        public Bouquet(int costBouq)
+        {
+            costOfBouquet = costBouq;
+        }
+        public override string ToString()
+        {
+            return $"Тип: {this.GetType()}, цена: {CostOfBouquet}";
+        }
+    }
+    [Serializable]
+    public class Rose : Bouquet
+    {
+        private string colorOfRose;
+        public string ColorOfRose
+        {
+            get { return colorOfRose; }
+            set { colorOfRose = value; }
+        }
+
+        public Rose(string color, int cost) : base(cost)
+        {
+            colorOfRose = color;
+        }
+        public override string ToString()
+        {
+            return $"Тип: {this.GetType()}, цена: {CostOfBouquet}, цвет роз: {ColorOfRose}";
+        }
+    }
+    [Serializable]
+    class Gladiolus : Bouquet
+    {
+        [NonSerialized]
+        private int numOfFlow;
+        public int NumOfFlow
+        {
+            get { return numOfFlow; }
+            set { numOfFlow = value; }
+        }
+        public Gladiolus(int num, int cost) : base(cost)
+        {
+            numOfFlow = num;
+        }
+        public override string ToString()
+        {
+            return $"Тип: {this.GetType()}, ценакак дела: {CostOfBouquet}, цвет роз: {NumOfFlow}";
+        }
+    }
+    [Serializable]
+    sealed class Paper : Bouquet
+    {
+        private string typeOfPaper;
+        public string TypeOfPaper
+        {
+            get { return typeOfPaper; }
+            set { typeOfPaper = value; }
+        }
+        public Paper(string type, int cost) : base(cost)
+        {
+            typeOfPaper = type;
+        }
+        public override string ToString()
+        {
+            return $"Тип: {this.GetType()}, цпривета: {CostOfBouquet}, вид бумаги: {TypeOfPaper}";
+        }
+    }
+}
---
+++ Lab13/Lab13/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.Serialization.Formatters;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Lab13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // задание 1
+            Rose forBin = new Rose("white", 400);
+            CustomSerializer.SerializeToBinary(forBin);
+            CustomSerializer.DeserializeFromBinary(ref forBin);
+            Console.WriteLine($"From .bin : {forBin.ToString()}");
+
+
+              Rose forSoap = new Rose("yellow", 500);
+              CustomSerializer.SerializeToSoap(forSoap);
+              CustomSerializer.DeserializeFromSoap(ref forSoap);
+
+            Rose forJson = new Rose("blue", 800);
+              CustomSerializer.SerializeToJson(forJson);
+              CustomSerializer.DeserializeFromJson(ref forJson);
+              Console.WriteLine($"From .json : {forBin.ToString()}");
+  
+            Bouquet forXml = new Bouquet();
+            CustomSerializer.SerializeToXml(forXml);
+            CustomSerializer.DeserializefromXml(ref forXml);
+            Console.WriteLine($"From .xml : {forXml.ToString()}");
+
+
+            // задание 3
+            List<Bouquet> forXML = new List<Bouquet>();
+            List<Bouquet> roses = new List<Bouquet>();
+
+            Bouquet first = new Bouquet();
+            Bouquet sec = new Bouquet();
+            Bouquet third = new Bouquet();
+
+            roses.Add(first);
+            roses.Add(sec);
+            roses.Add(third);
+
+
+            CustomSerializer.SerializeToXml(roses);
+            CustomSerializer.DeserializefromXml(ref forXML);
+            foreach (var temp in forXML)
+            {
+                Console.WriteLine(temp.ToString());
+            }
+
+            //задание 4
+            //используя XPath напишите два селектора для вашего XML-документа
+
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(@"D:\Универ\2курс\OOP_3sem\Lab13\Lab13\XML.xml");
+            var xRoot = xmlDoc.DocumentElement;
+
+            var selectNodes = xRoot.SelectNodes("*");
+            foreach (object xnode in selectNodes) Console.WriteLine((xnode as XmlNode).InnerText);
+
+            Console.WriteLine();
+            var nameNodes = xRoot.SelectNodes("Name");
+            foreach (object nameNode in nameNodes) Console.WriteLine((nameNode as XmlNode).Name);
+
+
+        }
+    }
+}
