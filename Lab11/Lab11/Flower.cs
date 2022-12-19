46ff92c9753fa3d96dbe14f7186c433e45bd25a3
Anastasiya Golodok <nastya.golodok@mail.ru>
2022-12-19 19:04:41 +0300

    Reflector, create, invoke

---
+++ Lab11/Lab11/Flower.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11
+{
+    internal class Flower
+    {
+        public string id;
+        public string type;
+        public string color;
+        public int price;
+
+        public static int countOfFlowers;
+
+        static Flower()
+        {
+            countOfFlowers = 0;
+        }
+
+        public Flower(string id, string type, string color, int price)
+        {
+            this.id = id;
+            this.type = type;
+            this.color = color;
+            this.price = price;
+            Flower.countOfFlowers++;
+        }
+
+        public Flower()
+        {
+            id = "Undefined";
+            type = "Undefined";
+            color = "Undefined";
+            price = 0;
+            Flower.countOfFlowers++;
+        }
+
+        public static void InformationClass()
+        {
+            Console.WriteLine($" - - - - - Information of Class - - - - - - - ");
+            Console.WriteLine($"Count flowers: {Flower.countOfFlowers}");
+
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null) return false;
+            if (obj.GetType() != this.GetType()) return false;
+            {
+                Flower flower = (Flower)obj;
+                return  (color == flower.color) && (type == flower.type);
+            }
+        }
+
+        public override string ToString()
+        {
+            return  "Type: " + type + ", Color: " + color;
+        }
+
+        public void WriteFlower(string typeFlower, Flower[] FLowerColection)
+        {
+            for (int i = 0; i < FLowerColection.Length; i++)
+            {
+                if (FLowerColection[i].type == typeFlower)
+                {
+                    Console.WriteLine($"{i + 1} flower: {FLowerColection[i].ToString()}");
+                }
+            }
+        }
+
+        public void WriteFlower(int temp, string type, Flower[] FLowerColection)
+        {
+            for (int i = 0; i < FLowerColection.Length; i++)
+            {
+                if (FLowerColection[i].type == type)
+                {
+                    if (FLowerColection[i].GetPrice() > temp)
+                    {
+                        Console.WriteLine($"{i + 1} car: {FLowerColection[i].ToString()}");
+                        Console.WriteLine($"Term of operation: {FLowerColection[i].GetPrice()}");
+                    }
+                }
+            }
+        }
+
+        public int GetPrice()
+        {
+            return  this.price - 10;
+        }
+    }
+}
---
+++ Lab11/Lab11/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Reflection;
+
+namespace Lab11
+{
+    static class A
+    {
+        //public void Anum() { }
+        public static void Byes(int i) { }
+
+        //public A() { }
+
+        static private int firstpub = 0;
+        static public int secondpr = 0;
+
+        static public string Third { get; set; }
+        static private string Fourth { get; set; }
+
+    }
+    class FlowerInvoke
+    {
+        //public void Anum() { }
+        public void WriteFlower(List<string> list)
+        {
+            foreach (var flower in list)
+            {
+                Console.WriteLine(flower);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            
+            Assembly assembly = Assembly.LoadFrom(@"D:\Универ\2курс\OOP_3sem\Lab04\Lab04\bin\Debug\netcoreapp3.1\Lab04.dll");
+            Console.WriteLine(assembly.FullName);
+            foreach (Module module in assembly.GetModules())
+            {
+                Console.WriteLine(module.FullyQualifiedName);
+                foreach (Type type in module.GetTypes())
+                {
+                    Console.WriteLine(type.FullName);
+                }
+            }
+            
+
+            Reflector.ExploreClass("Lab11.Flower");
+            Reflector.ExploreClass("Lab11.Employees");
+
+            Console.WriteLine("\nТест метода Invoke:\n");
+            Reflector.Invoke("Lab11.FlowerInvoke", "WriteFlower");
+
+            Console.WriteLine("\nТест метода Create:\n");
+
+            object obj = Reflector.Create("Lab11.Flower");
+            Console.WriteLine(obj);
+        }
+    }
+}
